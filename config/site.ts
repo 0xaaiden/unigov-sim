9a569f3e005b86e566cbf9c1dbb02f725dad9424
Aziz Aiden <azizaidencontact@gmail.com>
2023-10-12 14:15:49 -0500

    add env vars

--- config/site.ts
+++ config/site.ts
@@ -15,6 +15,11 @@ export const GOVERNANCE_CONTRACTS: GovernanceContract[] = [
     token: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
     label: 'Uniswap Governor Bravo (0x408ED...)',
   },
+  {
+    value: process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT && process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT !== '' ? process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT : undefined,
+    token: process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN && process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN !== '' ? process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN : undefined,
+    label: process.env.NEXT_PUBLIC_GOVERNANCE_LABEL && process.env.NEXT_PUBLIC_GOVERNANCE_LABEL !== '' ? process.env.NEXT_PUBLIC_GOVERNANCE_LABEL : undefined,
+  }
 ]
 
 export const initialValues: FormItems = {
