c14f6501ca43a5eb01b1e45eb107522321e5c365
Aziz Aiden <azizaidencontact@gmail.com>
2023-10-12 14:30:48 -0500

    edit config

--- config/site.ts
+++ config/site.ts
@@ -11,15 +11,19 @@ export type GovernanceContract = {
 }
 export const GOVERNANCE_CONTRACTS: GovernanceContract[] = [
   {
-    value: '0x408ed6354d4973f66138c91495f2f2fcbd8724c3',
-    token: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
-    label: 'Uniswap Governor Bravo (0x408ED...)',
+    value:
+      process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT && process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT !== ''
+        ? process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT
+        : undefined,
+    token:
+      process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN && process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN !== ''
+        ? process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN
+        : undefined,
+    label:
+      process.env.NEXT_PUBLIC_GOVERNANCE_LABEL && process.env.NEXT_PUBLIC_GOVERNANCE_LABEL !== ''
+        ? process.env.NEXT_PUBLIC_GOVERNANCE_LABEL
+        : undefined,
   },
-  {
-    value: process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT && process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT !== '' ? process.env.NEXT_PUBLIC_GOVERNANCE_CONTRACT : undefined,
-    token: process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN && process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN !== '' ? process.env.NEXT_PUBLIC_GOVERNANCE_TOKEN : undefined,
-    label: process.env.NEXT_PUBLIC_GOVERNANCE_LABEL && process.env.NEXT_PUBLIC_GOVERNANCE_LABEL !== '' ? process.env.NEXT_PUBLIC_GOVERNANCE_LABEL : undefined,
-  }
 ]
 
 export const initialValues: FormItems = {
